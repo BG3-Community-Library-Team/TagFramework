383d5f8c81023b3083de4e7df4daffd3ba815873
NellsRelo <Nellsrelo@gmail.com>
2024-09-05 14:05:12 -0700

    Ensure Reason field is given for non-REALLY tags

--- TagFramework/Mods/TagFramework/ScriptExtender/Lua/Actions/BG3SX_Support.lua
+++ TagFramework/Mods/TagFramework/ScriptExtender/Lua/Actions/BG3SX_Support.lua
@@ -1,15 +1,16 @@
-function Actions.AddToBG3SXList(tag, reallyTag, params)
+local function insertToBg3sxList(tag, params)
   local bg3sxList = Mods.BG3SX.Data.ModdedTags[params.RaceModGuid]
   bg3sxList[tag.Name] = {
     TAG = tag.ID,
-    Allowed = params.Allowed
+    Allowed = params.Allowed,
+    Reason = params.Reason or ""    
   }
+end
+
+function Actions.AddToBG3SXList(tag, reallyTag, params)
+  insertToBg3sxList(tag, params)
 
   if params.IncludeReally then
-    bg3sxList[reallyTag.Name] = {
-      TAG = reallyTag.ID,
-      Allowed = params.Allowed,
-      Reason = params.Reason or ""
-    }
+    insertToBg3sxList(reallyTag, params)
   end
 end
